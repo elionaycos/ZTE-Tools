03251a2f8f5962c32c7ef67282d51e52141f53d8
elionaycos <elionaycos2206@gmail.com>
2025-05-03 23:17:42 -0300

    add suport decrypt config.bin

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-#!.env/bin/python3
+#!./env/bin/python3
 
-from Cryptodome.Cipher import AES
-from Cryptodome.Util import Padding
+from hashlib import sha256
+from Crypto.Cipher import AES
+from Crypto.Util import Padding
 from io import BytesIO
 import struct
 import zlib
 import argparse
+from data import infile_bytes
 
 
 class ZTE_TOOLS:
@@ -21,81 +23,96 @@ class ZTE_TOOLS:
             type=str,
             help="Chave AES Do roteador"
             )
+        parser.add_argument(
+            "--iv",
+            type=str,
+            help="Chave IV Do roteador"
+            )
         
         args = parser.parse_args()
         
-        self.key = args.key
+        self.aes_key = args.key
         self.infile = args.infile
+        self.aes_iv = args.iv
     
+    def save(self,name_path,data):
+        data = data.read()
+        if isinstance(data,bytes):
+            data = bytes(data).decode()
+        with open(name_path,"w") as f:
+            f.write(str(data))
+        
+    
+    def read_header(self):
+        self.header = self.infile.read(206)
+        self.infile.seek(206)
+        print("Skip 206")
+        
     def set_key(self):
-        if not isinstance(self.key,bytes):
-            aes_key = self.key.encode()
-        else:
-            aes_key = self.key
+        if isinstance(self.aes_key,str):
+            self.aes_key = self.aes_key.encode()
+                
+        if self.aes_iv is None:
+            self.aes_iv = self.aes_key
+        elif isinstance(self.aes_iv,str):
+            self.aes_iv = self.aes_iv.encode()
+        
+        key = sha256(self.aes_key).digest()
+        iv = sha256(self.aes_iv).digest()
+        self.aes_cipher = AES.new(key,AES.MODE_CBC,iv[:16])
             
-        aes_key = aes_key.ljust(16,b"\0")[:16]
-        self.aes_cipher = AES.new(aes_key,AES.MODE_ECB)
-    
-    def decompress(self):
+            
+    def decompress(self,infile):
+        infile.seek(60)
         dec_data = BytesIO()
         crc = 0
         while True:
-            aes_header = struct.unpack(">3I",self.infile.read(12))
-            if len(aes_header)<3:
-                break
+            aes_header = struct.unpack(">3I",infile.read(12))
             
             dec_length = aes_header[0]
             com_length = aes_header[1]
-            cum_length = aes_header[2]
-            
-            com_chunk = self.infile.read(com_length)
-            if len(com_chunk) != com_length:
-                print(f"Erro: Esperado {com_length} bytes, mas leu {len(com_chunk)} bytes")
-                break
-            
+            com_chunk = infile.read(com_length)
             crc = zlib.crc32(com_chunk,crc)
-            try:
-                dec_chunk = zlib.decompress(com_chunk)
-            except zlib.error as e:
-                print(e)
-                break
-            assert dec_length == len(dec_chunk), f"Erro: comprimento descomprimido não bate {dec_length} != {len(dec_chunk)}"
-            
+            dec_chunk = zlib.decompress(com_chunk)
+            assert dec_length == len(dec_chunk),"Error ao ler dados tamanho %i vs %i"%(dec_length,len(dec_chunk))
             dec_data.write(dec_chunk)
-            if cum_length == 0:
+            if aes_header[2] == 0:
                 break
-            
-            dec_data.seek(0)
-            return dec_data,crc
+        
+        dec_data.seek(0)
+        return (dec_data,crc)
+        
         
     def load_chunk(self):
-        self.set_key()
         enc_data = BytesIO()
         tt_dec_size = 0
         while True:
-            header = self.infile.read(12)
-            if len(header) < 12:
-                print("Invalid header")
+            try:
+                dec_size,chunk_size,more_data = struct.unpack(">3I",self.infile.read(12))
+            except:
                 break
+            enc_data.write(self.infile.read(chunk_size))
+            tt_dec_size+=dec_size
             
-            c_size,d_size,m_chunks = struct.unpack(">3I",header)
-            enc_data.write(self.infile.read(c_size))
-            tt_dec_size += d_size
-            if m_chunks == 0:
+            if more_data == 0:
                 break
-            enc_data.seek(tt_dec_size)
-            return enc_data
+        enc_data.seek(tt_dec_size)
+        
+        return enc_data
     
     def decrypt(self):
+        self.set_key()
         data = self.load_chunk()
         data_size = data.tell()
-        d = Padding.pad(data.read(),AES.block_size)
         data.seek(0)
         res = BytesIO()
-        res.write(self.aes_cipher.decrypt(d))
+        res.write(self.aes_cipher.decrypt(data.read())[:data_size])
         res.seek(0)
         return res
 
 if __name__ == "__main__":
     zTools = ZTE_TOOLS()
-    res = zTools.decompress()
+    zTools.read_header()
+    data_compress = zTools.decrypt()
+    res,_ = zTools.decompress(data_compress)
+    zTools.save("config_decrypt.xml",res)
